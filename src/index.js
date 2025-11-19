d23cd5c44d6ecbcef61d60cf18fb75a55acb69c8
Orchard <rjbatson1@gmail.com>
2025-11-19 20:13:08 +1100

    beanstalking

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const app = express();
 const port = 8080;
+require("dotenv").config();
 
 app.get("/", (req, res) => {
-    res.send("Hello world");
+    res.send(`Hello world ${process.env.CUSTOM_VAR}`);
 })
 
 app.listen(port, () => {
